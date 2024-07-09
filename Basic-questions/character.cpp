9dc0b9e4106e182266b9dfae806dd7bc3e56bed4
PramodRawat <rawatpramod392@gmail.com>
2024-07-09 23:02:33 +0530

    add character question

---
+++ Basic-questions/character.cpp
@@ -0,0 +1,18 @@
+// print characters in small and letter
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    char ch;
+    cout << "small letters : ";
+    for(char i=97; i<=122; i++){
+        cout << i << ", ";
+    }
+    cout << endl;
+    cout << "chapital letters : ";
+    for(char i=65; i<=90; i++){
+        cout << i << ", ";
+    }
+    return 0;
+}
