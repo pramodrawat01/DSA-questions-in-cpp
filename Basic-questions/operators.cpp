9f2730f82d84b209271420e06934b0485c3cf089
PramodRawat <rawatpramod392@gmail.com>
2024-07-12 18:56:29 +0530

    add exor operator and complement operator

--- Basic-questions/operators.cpp
+++ Basic-questions/operators.cpp
@@ -1,9 +1,11 @@
-// Q. print character is vowel or consonant
+// OPERATORS IN C++
+
 #include<iostream>
 using namespace std;
 
 int main(){
     //Logical operator( &&(and), ||(or), !(not) )
+    // Q. print weather character is vowel or consonant
     char character;
     cout << "Enter any character" << endl;
     cin >> character;
@@ -15,9 +17,17 @@ int main(){
     }
 
     //Bitwise operator ( &(and), |(or), ^(exor), ~(compliment), <<(left shift), >>(right shift))
+    // '&' (and operator)
     int num = 2&3;  //convert both numbers into bits and then use 'and' operator
     cout << "bitwise operator : " << num << endl;
 
+    // '^' (Exor operator)  
+    int num1 = 2^3;  // convert into bits and 0 for same operands and 1 for different.
+    cout << "Exor operator : " << num1 << endl;  
+
+    // '~' (Complement operator)
+    int num2 = ~5; // convert all 1's bits into 0 and 0's bits into 1 and then find 2's complement for answer
+    cout << "Complement operator : " << num2 << endl;
 
 
     return 0;
