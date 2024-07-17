41d00e1e87050f9577a0a70d551264cd5a850f2b
PramodRawat <rawatpramod392@gmail.com>
2024-07-17 23:26:02 +0530

    questions added

--- Basic-questions/Questions/binary.cpp
+++ Basic-questions/Questions/binary.cpp
@@ -1,7 +1,25 @@
 // convert binary numbers to decimal and decimal numbers to binary
 #include<iostream>
 using namespace std;
-int main(){
+int main()
+{
+    int num;
+    cout << "Enter the number : ";
+    cin >> num;
+    int rem, ans = 0, mul=1;
+
+    while(num>0){
+        //reminder
+        rem = num%2;
+        //quotient
+        num = num/2;
+        // answer
+        ans = rem*mul+ans;
+        //mul
+        mul = mul*10;
+    }
+
+    cout << ans << endl;
     
     return 0;
 }
